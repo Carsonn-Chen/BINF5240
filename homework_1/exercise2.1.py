f774543e855fc4fed15b94c3d0591c8d8c7ba15c
carson <a1015403703@outlook.com>
2023-08-29 22:05:41 -0400

    upload homework

--- homework_1/exercise2.1.py
+++ homework_1/exercise2.1.py
@@ -2,3 +2,11 @@ codon = "ATG"
 
 lower_codon = codon.lower()
 print(lower_codon)
+
+lower_codon_backwards = lower_codon[-1] + lower_codon[-2] + lower_codon[-3]
+print(lower_codon_backwards)
+
+'''
+tmp = lower_codon[-1:-4:-1]
+print(tmp)
+'''
--- homework_1/exercise2.2.py
+++ homework_1/exercise2.2.py
@@ -1,7 +1,7 @@
 start_codon = "ATG"
 sequence = "gcatcacgttatgtcgactctgtgtggcgtctgctggg"
 
-pos = sequence.find(start_codon.lower())
-print(pos)
-pos_frame = pos % 3
-print(pos_frame)
+pos = sequence.find(start_codon.lower())+1
+print("The position of the first start-codon in the sequence is ", pos)
+pos_frame = pos%3
+print("The translation frame of the first start-codon in the sequence is ", pos_frame)
