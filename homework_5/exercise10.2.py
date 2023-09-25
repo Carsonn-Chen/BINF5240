0286a88c35e496318ce7709a247e550ad9a488b2
Carsonn <a1015403703@outlook.com>
2023-09-25 10:50:48 -0400

    modify exercise10.2.py

--- homework_5/exercise10.2.py
+++ homework_5/exercise10.2.py
@@ -1,16 +1,16 @@
+seq = "TTGAGTAGACGAAGAGGTGTCATGTCAAATCAATTTAAAGAAGAGCTTGCAAAAGAGCTAGGCTTTTATGATGTTGTTCAGAAAGAAGGATGGGGCGGAATTCGTGCGAAAGATGCTGGTAACATGGTGAAACGTGCTATAGAAATTGCAGAACAGCAATTAATGAAACAAAACCAGTAGXYZN"
+
 def count_nucleotides(seq):
     nucleotide_counts = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
 
-    for base in seq:
-        if base in nucleotide_counts:
-            nucleotide_counts[base] += 1
+    for i in seq:
+        nucleotide_counts[i] = nucleotide_counts.get(i, 0)+1
 
     sorted_counts = sorted(nucleotide_counts.items(), key=lambda x: x[1], reverse=True)
+    return sorted_counts
 
-    for base, count in sorted_counts:
+
+sorted_counts = count_nucleotides(seq)
+for base, count in sorted_counts:
         print(f'{base}: {count}')
 
-
-# Example usage:
-input_sequence = "ATGCTAGCTAGCTAGCTA"
-count_nucleotides(input_sequence)
